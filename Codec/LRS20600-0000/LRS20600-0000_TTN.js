3af1efd46dc0523c47b2c64f0d0d53adbec34ea3
arthur.lai <arthur.lai@arwintechnology.com>
2025-06-26 15:14:09 -0400

    Added pulse count mode for LRS20600-0000 TTN decoder.

--- Codec/LRS20600-0000/LRS20600-0000_TTN.js
+++ Codec/LRS20600-0000/LRS20600-0000_TTN.js
@@ -1,3 +1,4 @@
+
 var lrs20600_events = ['heartbeat/button', 'rsvd', 'rsvd', 'rsvd', 'rsvd', 'rsvd', 'rsvd', 'rsvd'];
 var trigger_mode = ['unknown', 'NC, falling edge trigger',        'NO, rising edge trigger',
                                'NC, rising/falling edge trigger', 'NO, rising/falling edge trigger'];
@@ -5,7 +6,7 @@ var trigger_mode = ['unknown', 'NC, falling edge trigger',        'NO, rising ed
 function hex2dec(hex) {
   var dec = hex&0xFFFF;
   if (dec & 0x8000)
-    dec = -(0x10000-dec)
+    dec = -(0x10000-dec);
   return dec;
 }
 
@@ -19,21 +20,35 @@ function decodeUplink(input) {
       switch (input.bytes[0]) {
         case 4:
           var evt= "";
-          for (let i=0; i<8; i++) {
-            if ((0x01<<i)&input.bytes[1]) 
+          for (var i=0; i<8; i++) {
+            if ((0x01<<i)&input.bytes[1])
               if (evt==="")
                 evt=lrs20600_events[i];
               else
                 evt=evt+","+lrs20600_events[i];
           }
-          return {
-            data: {
-              event: evt,
-              battery: input.bytes[2],
-              inputStatus: input.bytes[3] ? "close" : "open",
-              eventCount: hex2dec(input.bytes[4]<<8|input.bytes[5]),
-            },
-          };
+          if (input.bytes.length > 6) {
+            var reading = ( input.bytes[6] << 24 | input.bytes[7] << 16 | input.bytes[8] << 8 | input.bytes[9] ) +
+                          ( input.bytes[4] << 8  | input.bytes[5] ) / Math.pow( 10, input.bytes[10] );
+            return {
+              data: {
+                event: evt,
+                battery: input.bytes[2],
+                inputStatus: input.bytes[3] ? "close" : "open",
+                meterReading: reading
+              }
+            };
+          }
+          else {
+            return {
+              data: {
+                event: evt,
+                battery: input.bytes[2],
+                inputStatus: input.bytes[3] ? "close" : "open",
+                eventCount: input.bytes[4]<<8|input.bytes[5]
+              }
+            };
+          }
         default:
           return {
             errors: ['unknown sensor type']
@@ -41,23 +56,20 @@ function decodeUplink(input) {
       }
       break;
     case 8: // version
-      var ver = input.bytes[0]+"."+("00"+input.bytes[1]).slice(-2)+"."+("000"+(input.bytes[2]<<8|input.bytes[3])).slice(-3);    
+      var ver = input.bytes[0]+"."+("00"+input.bytes[1]).slice(-2)+"."+("000"+(input.bytes[2]<<8|input.bytes[3])).slice(-3);
       return {
         data: {
-          firmwareVersion: ver,
+          firmwareVersion: ver
         }
       };
-    case 9: // status
-      var ver = input.bytes[0]+"."+("00"+input.bytes[1]).slice(-2)+"."+("000"+(input.bytes[2]<<8|input.bytes[3])).slice(-3);    
-      var battery_level = (input.bytes[4]<<8 | input.bytes[5]);
-      var battery_percentage = input.bytes[6];
-      var uplink_count = input.bytes[7]<<24 | input.bytes[8]<< 16 | input.bytes[9]<<8 | input.bytes[10];
+    case 9: // version, battery & uplink count
+      var ver = input.bytes[0]+"."+("00"+input.bytes[1]).slice(-2)+"."+("000"+(input.bytes[2]<<8|input.bytes[3])).slice(-3);
       return {
         data: {
           firmwareVersion: ver,
-          batteryLevel: battery_level,
-          batteryPercentage: battery_percentage,
-          uplinkCount: uplink_count
+          batteryLevel: input.bytes[4]<<8|input.bytes[5],
+          batteryPercentage: input.bytes[6],
+          uplinkCount: input.bytes[7]<<24|input.bytes[8]<<16|input.bytes[9]<<8|input.bytes[10]
         }
       };
     case 12: // device settings
@@ -67,31 +79,51 @@ function decodeUplink(input) {
             data: {
               dataUploadInterval: hex2dec(input.bytes[1]<<8|input.bytes[2]),
               triggerMode: (input.bytes[3] < 5) ? trigger_mode[input.bytes[3]] : trigger_mode[0],
-              triggerDeafTime: hex2dec(input.bytes[4]<<8|input.bytes[5]),
+              triggerDeafTime: hex2dec(input.bytes[4]<<8|input.bytes[5])
             }
           };
         default:
           return {
             errors: ['unknown sensor type']
-          }
+          };
       }
-      case 13: // threshold settings
+      break;
+    case 13: // dry contact settings
       switch (input.bytes[0]) {
         case 4:
-          var state = input.bytes[1] ? 'enable': 'disable'
+          var state = input.bytes[1] ? 'enable': 'disable';
           return {
             data: {
-              inputEnabled: state,
+              inputEnabled: state
             }
-          }
+          };
         default:
           return {
             errors: ['unknown sensor type']
-          }
+          };
       }
+      break;
+    case 16: // pulse counter mode settings
+      switch (input.bytes[0]) {
+        case 4:
+          return {
+            data: {
+              pulseCountMode: input.bytes[1] ? 'enable' : 'disable',
+              maxDigit: input.bytes[2],
+              resolution: input.bytes[3],
+              initialReadingInteger: input.bytes[4] << 24 | input.bytes[5] << 16 | input.bytes[6] << 8 | input.bytes[7],
+              initialReadingDecimal: input.bytes[8] << 8 | input.bytes[9]
+            }
+          };
+        default:
+          return {
+            errors: ['unknown sensor type']
+          };
+      }
+      break;
     default:
       return {
-        errors: ['unknown FPort'],
+        errors: ['unknown FPort']
       };
   }
 }
@@ -111,7 +143,7 @@ function encodeDownlink(input) {
       bytes: [0x04, 0x00]
     };
   }
-  else if (input.data.cmd === 'getInputSettings') {
+  else if (input.data.cmd === 'getThresholdSettings') {
     return {
       fPort: 21,
       bytes: [0x04, 0x01]
@@ -131,15 +163,15 @@ function encodeDownlink(input) {
                               (deaf>>8)&0xff,deaf&0xff,
                               dack)
       };
-    };
+    }
   }
   else if (input.data.cmd === 'setInputConfiguration') {
     var en = input.data.inputEnable;
-    if ((en === 'enable') | (en === 'disable')) {
+    if ((en === 'enable') || (en === 'disable')) {
       return {
         fPort: 23,
         bytes: payload.concat(0x04, (en === 'enable'))
       };
-    };
+    }
   }
 }
